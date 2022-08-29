7a72608699c9ea9edd05a573d497ec1edda45395
shchon <shchon1993@naver.com>
2022-08-29 15:47:21 +0900

    문자열 문제 3

---
+++ src/string3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class string3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String arr[] = str.split(" ");
+
+
+
+        int com = 0;
+        String result = arr[com];
+
+        for(int i = 1; i<arr.length;i++){
+            if(arr[com].length() < arr[i].length()){
+                com = i;
+                result = arr[i];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
