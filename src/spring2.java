68447d4b7612e3e5b8690e214a27423aef3abf00
shchon <shchon1993@naver.com>
2022-08-29 15:27:13 +0900

    문자열 문제 2

---
+++ src/spring2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class spring2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String result = "";
+
+        for(int i = 0; i<str1.length();i++){
+            int check = str1.charAt(i) - '0';
+
+            if(check >= 49 && check <= 74){
+                char ch1 = Character.toUpperCase(str1.charAt(i));
+                result += String.valueOf(ch1);
+                //System.out.println("소문자");
+            }
+            else if(check >= 17 && check <= 42){
+                char ch2 = Character.toLowerCase(str1.charAt(i));
+                result += String.valueOf(ch2);
+                //System.out.println("대문자");
+            }
+        }
+        System.out.println(result);
+        //a 49 A 17
+        //z 74 Z 42
+    }
+
+}
